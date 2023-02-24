61719185906d819b634b68d0bc631a26c98eaaaf
jdussold <jdussold@gmail.com>
2023-02-24 14:15:04 -0600

    updated to set background color dynamically

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default class Chat extends React.Component {
   componentDidMount() {
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
+    // Retrieve the name and color values from the navigation prop
+    let name = this.props.route.params.name;
+    let color = this.props.route.params.color;
+
+    // Set the header title to the name value
     this.props.navigation.setOptions({ title: name });
+
+    // Set the header background color to the color value
+    this.props.navigation.setOptions({
+      headerStyle: {
+        backgroundColor: color,
+      },
+    });
   }
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View
+        style={[
+          styles.container,
+          // Set the background color of the chat screen to the color value
+          { backgroundColor: this.props.route.params.color },
+        ]}
+      >
         <Button
           title="Go to Start"
           onPress={() => this.props.navigation.navigate("Start")}
@@ -19,3 +35,11 @@ export default class Chat extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
