c8a366ae32bc3add9117fbfc1bfc3807338879e4
jdussold <jdussold@gmail.com>
2023-03-11 16:01:04 -0600

    Implemented GiftedChat library

--- components/Chat.js
+++ components/Chat.js
@@ -1,45 +1,98 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Button,
+  StyleSheet,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
-export default class Chat extends React.Component {
-  componentDidMount() {
+const renderBubble = (props) => {
+  return (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: "#000",
+        },
+        left: {
+          backgroundColor: "#FFF",
+        },
+      }}
+    />
+  );
+};
+
+export default function Chat(props) {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placebear.com/140/140",
+        },
+      },
+      {
+        _id: 2,
+        text: "You've entered the chat",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  useEffect(() => {
     // Retrieve the name and color values from the navigation prop
-    let name = this.props.route.params.name;
-    let color = this.props.route.params.color;
+    let name = props.route.params.name;
+    let color = props.route.params.color;
 
     // Set the header title to the name value
-    this.props.navigation.setOptions({ title: name });
+    props.navigation.setOptions({ title: name });
 
     // Set the header background color to the color value
-    this.props.navigation.setOptions({
+    props.navigation.setOptions({
       headerStyle: {
         backgroundColor: color,
       },
     });
-  }
+  }, [props.route.params.name, props.route.params.color]);
 
-  render() {
-    return (
-      <View
-        style={[
-          styles.container,
-          // Set the background color of the chat screen to the color value
-          { backgroundColor: this.props.route.params.color },
-        ]}
-      >
-        <Button
-          title="Go to Start"
-          onPress={() => this.props.navigation.navigate("Start")}
-        />
-      </View>
+  const onSend = (newMessages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
     );
-  }
+  };
+
+  return (
+    <View
+      style={[styles.container, { backgroundColor: props.route.params.color }]}
+    >
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(newMessages) => onSend(newMessages)}
+        user={{ _id: 1 }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+      <Button
+        title="Leave Chat"
+        onPress={() => props.navigation.navigate("Start")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
