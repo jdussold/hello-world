42e9fdb282d81af364be6f8f68685885a9857744
jdussold <jdussold@gmail.com>
2023-03-17 00:42:04 -0500

    Updated to implement firebase data storage

--- App.js
+++ App.js
@@ -1,33 +1,46 @@
-// Import React and necessary components from react-native
-import React, { Component } from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
-
-// Import Start and Chat components from components directory
+// Import libraries and components
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { LogBox } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
-// Import react-native gesture handler library
-import "react-native-gesture-handler";
-
-// Import NavigationContainer and createStackNavigator from react-navigation library
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+// Ignore expo warning messages
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 // Create a navigator using createStackNavigator
 const Stack = createStackNavigator();
 
-// Define the App component as a subclass of React.Component
-export default class App extends React.Component {
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD80gTYOsQnNFBXLsJH0BW0rKUy-tmHyPQ",
+    authDomain: "hello-world-c79f6.firebaseapp.com",
+    projectId: "hello-world-c79f6",
+    storageBucket: "hello-world-c79f6.appspot.com",
+    messagingSenderId: "351256636874",
+    appId: "1:351256636874:web:c91cdee283f73310153333",
+  };
+
+  // Initialize Firebase app
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   // Render the app with NavigationContainer and Stack.Navigator
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Start">
-          {/* Define two screens using Stack.Screen, with Start and Chat components as their respective components */}
-          <Stack.Screen name="Start" component={Start} />
-          <Stack.Screen name="Chat" component={Chat} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        {/* Define two screens using Stack.Screen, with Start and Chat components as their respective components */}
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
